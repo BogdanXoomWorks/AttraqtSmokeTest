18eee2eebed617d03109b5d5f0ec5bc9d3a5a3c9
BogdanXoomWorks <35062322+BogdanXoomWorks@users.noreply.github.com>
2018-01-23 14:07:24 +0200

    MisspelledSearch, Search Terms, Product Performance tests - DONE

--- src/main/java/com/xoomworks/attraqt/suites/SmokeTest.java
+++ src/main/java/com/xoomworks/attraqt/suites/SmokeTest.java
@@ -3,6 +3,7 @@ package com.xoomworks.attraqt.suites;
 
 import com.xoomworks.attraqt.pageobjects.ProductPerformance;
 import com.xoomworks.attraqt.tests.MisspelledSearchTermsTest;
+import com.xoomworks.attraqt.tests.ProductPerformanceTest;
 import com.xoomworks.attraqt.tests.SearchTermsTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -11,7 +12,7 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
         MisspelledSearchTermsTest.class,
         SearchTermsTest.class,
-        ProductPerformance.class
+        ProductPerformanceTest.class
 })
 public class SmokeTest {
 }
